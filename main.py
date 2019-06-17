8b10dd0f838871d0937447475c9df2fed97cb822
clandero <clandero@udec.cl>
2019-06-16 23:17:31 -0400

    Pet API fetching by specie

--- main.py
+++ main.py
@@ -5,14 +5,33 @@ from marshmallow import Schema, fields
 import werkzeug
 import logging
 import requests
+import httplib
 import json
 import sys
+import requests_toolbelt.adapters.appengine
+
+# Use the App Engine Requests adapter. This makes sure that Requests uses
+# URLFetch.
+requests_toolbelt.adapters.appengine.monkeypatch()
 
 app = Flask(__name__)
 app.debug = True
 
+@app.route('/pets')
+def get_pets():
+	r = requests.get('http://practiceiv-on-gcloud.appspot.com/products/fetch')
+	print(r.text,file=sys.stdout)
+	return render_template('pets.html',products=json.loads(r.text)['products'])
+	#return render_template('pets.html')
+
+@app.route('/pets/cats')
+def get_cats():
+	r = requests.get('http://practiceiv-on-gcloud.appspot.com/products/get/specie/Cats')
+	print(r.text,file=sys.stdout)
+	return render_template('pet_specie.html',products=json.loads(r.text)['Cats'],specie="Cats")
+
 @app.route('/')
 def index():
-	r = requests.get('http://practiceiv-on-gcloud.appspot.com/products/fetch')
-	return render_template('index.html',products=json.loads(r.text)['products'])
-	
+	return render_template('index.html')
+
+
